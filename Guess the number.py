3292af1f48f5a114a84cc03eccde49d176671a75
Aivoric85 <109034917+Aivoric85@users.noreply.github.com>
2022-07-11 06:19:52 +0200

    Add files via upload

--- Guess the number.py
+++ Guess the number.py
@@ -1,29 +1,24 @@
 import random
 
 
-# function generates a random number between 0 and 100
-def generate_number():
-    number = random.randrange(0, 100)
-    return number
-
-
-# main function to let the user guess the generated number
+# main function to generate a random number the user has to guess
 def guess_the_number():
+    number = random.randrange(0, 100)
     while True:
         g = input("Bitte gib eine Zahl zwischen 0 und 100 eingeben. ")
         try:
             guess = int(g)
             if guess > number:
-                a1 = random.choice(["Es tut mir leid, leider ist die von dir eingegebene Zahl zu hoch.",
-                                    "Tut mir leid " + name + ", leider zu hoch.",
-                                    name + ", die Zahl ist leider zu hoch.",
-                                    "Leider ist die von dir eingegebene Zahl zu hoch."])
+                a1 = random.choice(["Leider ist die von dir eingegebene Zahl zu hoch.Versuche es noch einmal",
+                                    "Tut mir leid " + name + ", leider zu hoch.Versuche es noch einmal",
+                                    name + ", die Zahl ist leider zu hoch.Versuche es noch einmal",
+                                    "Leider ist die von dir eingegebene Zahl zu hoch.Versuche es noch einmal"])
                 print(a1)
             if guess < number:
-                a2 = random.choice(["Es tut mir leid, leider ist die von dir eingegebene Zahl zu niedrig.",
-                                    "Schade " + name + ", leider zu niedrig.",
-                                    name + " ,leider ist deine Zahl zu niedrig.",
-                                    "Die von dir eingegebene Zahl ist leider zu niedrig."])
+                a2 = random.choice(["Leider ist die von dir eingegebene Zahl zu niedrig.Versuche es noch einmal",
+                                    "Schade " + name + ", leider zu niedrig.Versuche es noch einmal",
+                                    name + " ,leider ist deine Zahl zu niedrig.Versuche es noch einmal",
+                                    "Die von dir eingegebene Zahl ist leider zu niedrig.Versuche es noch einmal"])
                 print(a2)
             if guess == number:
                 print("Herzlichen Glückwunsch " + name + " ,du hast die richtige Zahl erraten!! " + str(number))
@@ -40,7 +35,7 @@ def new_round():
         if answer in ["ja", "j", "Ja", "J"]:
             print("Okay " + name + ", neue Runde")
             guess_the_number()
-        if answer in ["Nein", "nein", "ne", "Ne"]:
+        if answer in ["Nein", "nein", "ne", "Ne", "N", "n"]:
             print("Auf wiedersehen " + name + ", bis zum nächsten mal")
             exit()
         else:
@@ -48,7 +43,7 @@ def new_round():
 
 
 if __name__ == '__main__':
-    name = input("Hallo, magst du mir deinen Namen verraten? ")
+    print("Hallo, lass uns ein Spiel spielen.Ich überlege mir eine Zahl und du musst raten welche es ist!")
+    name = input("Magst du mir deinen Namen verraten? ")
     print("Guten Tag " + name + ". Lass uns anfangen!")
-    number = generate_number()
     guess_the_number()

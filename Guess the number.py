68c902e5e2f9b4ff17bc59c9b1213a87ffa0a8cc
Aivoric85 <109034917+Aivoric85@users.noreply.github.com>
2022-07-23 16:46:03 +0200

    Update Guess the number.py

--- Guess the number.py
+++ Guess the number.py
@@ -1,72 +1,71 @@
 import random
 
 
-# main function to generate a random number the user has to guess in german
-def guess_the_number_de(name):
-    print("Ich überlege mir eine Zahl zwischen 0 und 100 und du musst raten welche es ist!")
+# main function to generate a random number the user has to guess in german and english
+def guess_the_number(name, lang):
+    if lang == "de":
+        print("Ich überlege mir eine Zahl zwischen 0 und 100 und du musst raten welche es ist!")
+    if lang == "en":
+        print("I got a number between 0 and 100 in mind. Can you guess witch?")
     number = random.randrange(0, 100)
     while True:
-        g = input("Bitte gib eine Zahl zwischen 0 und 100 eingeben. \n")
-        try:
-            guess = int(g)
-            if guess > number:
+        if lang == "de":
+            try:
+                guess = int(input("Bitte gib eine Zahl zwischen 0 und 100 eingeben. \n"))
+            except ValueError:
+                print("Bitte nur Zahlen von 0 bis 100 verwenden.")
+        if lang == "en":
+            try:
+                guess = int(input("Please enter a number between 0 and 100. \n"))
+            except ValueError:
+                print("Please only use numbers between 0 and 100.")
+        if guess > number:
+            if lang == "de":
                 print(random.choice(["Leider ist die von dir eingegebene Zahl zu hoch. Versuche es noch einmal.",
                                      "Tut mir leid " + name + ", leider zu hoch. Versuche es noch einmal.",
                                      name + ", die Zahl ist leider zu hoch. Versuche es noch einmal.",
                                      "Leider ist die von dir eingegebene Zahl zu hoch. Versuche es noch einmal."]))
-            if guess < number:
+            if lang == "en":
+                print(random.choice(["Sorry, the number you have entered is to high. Please try again.",
+                                     "Sorry " + name + ", to high. Please try again.",
+                                     name + ", the number you picked is to high. Please try again.",
+                                     "The number you have chosen is to high. Please try again."]))
+        if guess < number:
+            if lang == "de":
                 print(random.choice(["Leider ist die von dir eingegebene Zahl zu niedrig. Versuche es noch einmal.",
                                      "Schade " + name + ", leider zu niedrig. Versuche es noch einmal.",
                                      name + ", leider ist deine Zahl zu niedrig. Versuche es noch einmal.",
                                      "Die von dir eingegebene Zahl ist leider zu niedrig. Versuche es noch einmal."]))
-            if guess == number:
+            if lang == "en":
+                print(random.choice(["Sorry, the number you have entered is to low. Please try again.",
+                                     "Sorry " + name + ", to low. Please try again.",
+                                     name + ", the number you picked is to low. Please try again.",
+                                     "The number you entered is to low... Sorry. Please try again."]))
+        if guess == number:
+            if lang == "de":
                 print("Herzlichen Glückwunsch " + name + ", du hast die richtige Zahl erraten!! " + str(number))
                 while True:
                     answer = input("Möchtest du es noch einmal versuchen? Tippe ja oder nein \n")
                     if answer in ["ja", "j", "Ja", "J"]:
                         print("Okay " + name + ", neue Runde")
-                        guess_the_number_de(name)
+                        guess_the_number(name, lang)
                     if answer in ["Nein", "nein", "ne", "Ne", "N", "n"]:
                         print("Auf Wiedersehen " + name + ", bis zum nächsten mal")
                         exit()
                     else:
                         print("Bitte mit ja oder nein antworten")
-        except ValueError:
-            print("Bitte nur Zahlen von 0 bis 100 verwenden.")
-
-
-# main function to generate a random number the user has to guess in english
-def guess_the_number_en(name):
-    print("I got a number between 0 and 100 in mind. Can you guess witch?")
-    number = random.randrange(0, 100)
-    while True:
-        g = input("Please enter a number between 0 and 100. \n")
-        try:
-            guess = int(g)
-            if guess > number:
-                print(random.choice(["Sorry, the number you have entered is to high. Please try again.",
-                                     "Sorry " + name + ", to high. Please try again.",
-                                     name + ", the number you picked is to high. Please try again.",
-                                     "The number you have chosen is to high. Please try again."]))
-            if guess < number:
-                print(random.choice(["Sorry, the number you have entered is to low. Please try again.",
-                                     "Sorry " + name + ", to low. Please try again.",
-                                     name + ", the number you picked is to low. Please try again.",
-                                     "The number you entered is to low... Sorry. Please try again."]))
-            if guess == number:
+            if lang == "en":
                 print("Congratulations " + name + "!! You got the correct number. " + str(number))
                 while True:
                     answer = input("Would you like to try again? Type yes or no. \n")
                     if answer in ["yes", "y"]:
                         print("Okay " + name + ". One more try!")
-                        guess_the_number_en(name)
+                        guess_the_number(name, lang)
                     if answer in ["no", "n"]:
                         print("Good bye " + name + ". See you next time.")
                         exit()
                     else:
                         print("Please use yes/y or no/n")
-        except ValueError:
-            print("Please only use numbers between 0 and 100.")
 
 
 while True:
@@ -74,10 +73,10 @@ while True:
     if lang == "de":
         name = input("Magst du mir deinen Namen verraten? \n")
         print("Guten Tag " + name + ". Lass uns anfangen!")
-        guess_the_number_de(name)
+        guess_the_number(name, lang)
     if lang == "en":
         name = input("Hi, would yo tell me your name?\n")
         print("Hello " + name + ". Let's get started!")
-        guess_the_number_en(name)
+        guess_the_number(name, lang)
     else:
         print("Ops, " + str(lang) + " is no valid option.")
